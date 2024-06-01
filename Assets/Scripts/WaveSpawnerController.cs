a8afea72b36215f7b6980da54200d432bea81c55
George Tzoupis <giwrgos0gt15@gmail.com>
2024-06-01 20:31:21 +0300

    added enemies with multiple difficulties

--- Assets/Scripts/WaveSpawnerController.cs
+++ Assets/Scripts/WaveSpawnerController.cs
@@ -1,49 +1,68 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using SOs;
 using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+internal enum EnemyType
+{
+    Basic = 0,
+    Medium = 1,
+    Hard = 2,
+}
+
 [Serializable]
 internal struct Wave
 {
     public int numBasicEnemies;
+    public int numMediumEnemies;
+    public int numHardEnemies;
     public float speedModifier;
     public float spawnRate;
+
+    public int numEnemies => numBasicEnemies + numMediumEnemies + numHardEnemies;
 }
 
 public class WaveSpawnerController : MonoBehaviour
 {
     [SerializeField] private Wave[] waves;
     [SerializeField] private GameObject basicEnemy;
+    [SerializeField] private GameObject mediumEnemy;
+    [SerializeField] private GameObject hardEnemy;
 
     private int currentWaveIx = 0;
     private float spawnTimer = 0.0f;
 
-    // Start is called before the first frame update
-    private void Start()
-    {
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-    }
-
     private void FixedUpdate()
     {
         if (currentWaveIx >= waves.Length) return;
-        
+
         spawnTimer -= Time.fixedDeltaTime;
         if (spawnTimer < 0)
         {
-            SpawnBasicEnemy(waves[currentWaveIx]);
-            waves[currentWaveIx].numBasicEnemies -= 1;
-            Debug.Log($"num enemies {waves[currentWaveIx].numBasicEnemies}");
+            var enemyType = SelectEnemyType(waves[currentWaveIx]);
+            switch (enemyType)
+            {
+                case EnemyType.Basic:
+                    SpawnEnemy(waves[currentWaveIx], basicEnemy);
+                    waves[currentWaveIx].numBasicEnemies -= 1;
+                    break;
+                case EnemyType.Medium:
+                    SpawnEnemy(waves[currentWaveIx], mediumEnemy);
+                    waves[currentWaveIx].numMediumEnemies -= 1;
+                    break;
+                case EnemyType.Hard:
+                    SpawnEnemy(waves[currentWaveIx], hardEnemy);
+                    waves[currentWaveIx].numHardEnemies -= 1;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
 
-            if (waves[currentWaveIx].numBasicEnemies == 0)
+            if (waves[currentWaveIx].numEnemies == 0)
             {
                 currentWaveIx++;
                 if (currentWaveIx >= waves.Length) return;
@@ -53,13 +72,40 @@ public class WaveSpawnerController : MonoBehaviour
         }
     }
 
-    private void SpawnBasicEnemy(Wave wave)
+    private static EnemyType SelectEnemyType(Wave wave)
     {
+        var numEnemies = new List<int>
+        {
+            wave.numBasicEnemies,
+            wave.numMediumEnemies,
+            wave.numHardEnemies
+        };
+
+        for (var i = 1; i < numEnemies.Count; ++i)
+        {
+            numEnemies[i] += numEnemies[i - 1];
+        }
+
+        var rand = Random.Range(0, numEnemies[numEnemies.Count - 1]);
+        for (var i = 0; i < numEnemies.Count; ++i)
+        {
+            if (rand <= numEnemies[i])
+            {
+                return (EnemyType)i;
+            }
+        }
+
+        throw new IndexOutOfRangeException("Couldn't select enemy type");
+    }
+
+    private void SpawnEnemy(Wave wave, GameObject enemyPrefab)
+    {
+        // Spawn enemy to random height
         var cam = Camera.main;
         var start = cam.ScreenToWorldPoint(Vector3.zero);
         var end = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
         var heightModifier = Random.Range(start.y, end.y) * 0.9f;
-        var enemy = Instantiate(basicEnemy, transform.position + Vector3.up * heightModifier, Quaternion.identity);
+        var enemy = Instantiate(enemyPrefab, transform.position + Vector3.up * heightModifier, Quaternion.identity);
         enemy.GetComponent<EnemyController>().AddMoveSpeed(Random.Range(0, wave.speedModifier));
     }
 }
